2e41398e00b5279447127e54b88a9fe44f8fa00e
Mario Andre <mario.andre.m@gmail.com>
2022-05-04 20:44:06 -0300

    pequenas adaptações para se adequar aos cards do desafio no trello

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let emailIsValid = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const emailField = document.querySelector('#email');
+    const buttonLogin = document.querySelector('#login');
+    const loginForm = document.querySelector('#login-form');
+    emailField.addEventListener('blur', (e) => {
+        validarEmail(e.target);
+    })
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('hello');
+        const passwordField = document.querySelector('#password');
+        if (passwordField.value.length > 0 && emailIsValid) {
+            window.location.href = "http://127.0.0.1:5500/adicionar-produto.html";
+        }
+
+    })
+})
+
+
+function validarEmail(input) {
+    const email = input.value;
+    let mensagem = '';
+    let padraoEsperado = /@\S+.\S+/;
+    if (!padraoEsperado.test(email)) {
+        mensagem = 'Digite um email no formato: endereco@provedor.com';
+    } else {
+        emailIsValid = true;
+    }
+    input.setCustomValidity(mensagem)
+}
